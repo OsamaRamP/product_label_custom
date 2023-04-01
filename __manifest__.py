86775fe8021ecb2262e60d21eb1271a66adf84b6
osamaleroi <osama.ramadan.p@gmail.com>
2023-04-01 05:51:19 +0200

    [add] version 16.0

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Products Labels Barcode ',
+    'summary': 'Products Labels Barcode ',
+    'version': '16.0.5',
+    'price': 0,
+    "author" : "Osama Ramadan",
+    'sequence': 1,
+    'license': 'OPL-1',
+    'category': 'product',
+    'depends': [
+        'product',
+    ],
+    'data': [
+        'report/product_product_templates.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
